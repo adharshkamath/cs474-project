e2c97b50bf7192fb3133e54c8d16be4f42d9d3ec
Adharsh Kamath <ak128@illinois.edu>
2024-12-09 19:03:39 -0600

    checkpoint

--- EuclidZ3/main.py
+++ EuclidZ3/main.py
@@ -12,64 +12,61 @@ solver.add(language.axioms)
 
 print("=== Finished Loading Core ===")
 
-# a b c = triangle a b c; p = on_line p b c, on_aline p a b b c a; q = on_line q b c, on_aline q a c c b a; m = mirror m a p; 
-# n = mirror n a q; x = on_line x b m, on_line x c n; o = circle o a b c ? cong o x o a
+# a b = segment a b; c = on_bline c a b; d = on_line d a c; e = eqdistance e b a d, on_line e b c; f = on_line f a b, on_line f d e; 
+# g = on_line g b c, on_pline g d a b ? cong d f f e
 
-A1, B1, C1, M1, N1, P1, Q1, X1, O1 = Consts('A1 B1 C1 M1 N1 P1 Q1 X1 O1', language.PointSort)
-solver.add(simplify(Distinct(A1,B1,C1,M1,N1,P1,Q1,X1,O1), blast_distinct=True))
+A1, B1, C1, D1, E1, F1 = Consts('A1 B1 C1 D1 E1 F1', language.PointSort)
+solver.add(simplify(Distinct(A1,B1,C1,D1,E1,F1), blast_distinct=True))
 
-AB1, BC1, CA1 = Consts('AB1 BC1 CA1', language.LineSort)
-solver.add(simplify(Distinct(AB1,BC1,CA1), blast_distinct=True))
-
-BM1, CN1 = Consts('BM1 CN1', language.LineSort)
-solver.add(simplify(Distinct(BM1,CN1), blast_distinct=True))
-
-OABC = Const('OABC', language.CircleSort)
-solver.add(simplify(Distinct(OABC), blast_distinct=True))
+BCA, FAE = Consts('BCA FAE', language.LineSort)
+solver.add(simplify(Distinct(BCA,FAE), blast_distinct=True))
 
 assumptions = []
 
-# assumptions.append(ForAll([A1, C1, AB1], Implies(And(language.OnLine(A1, AB1), Not(language.OnLine(C1, AB1))), Not(A1 == C1))))
+assumptions.append(language.Segment(C1, A1) == language.Segment(C1, B1))
 
-assumptions.append(language.OnLine(A1,AB1))
-assumptions.append(language.OnLine(B1,AB1))
-assumptions.append(Not(language.OnLine(C1,AB1)))
+assumptions.append(language.OnLine(A1,BCA))
+assumptions.append(language.OnLine(B1,BCA))
+assumptions.append(language.OnLine(C1,BCA))
 
-assumptions.append(language.OnLine(B1,BC1))
-assumptions.append(language.OnLine(C1,BC1))
-assumptions.append(Not(language.OnLine(A1,BC1)))
+assumptions.append(language.Segment(C1, D1) == language.Segment(C1, A1))
+assumptions.append(language.Angle(D1, B1, A1) == language.Angle(E1, B1, D1))
 
-assumptions.append(language.OnLine(C1,CA1))
-assumptions.append(language.OnLine(A1,CA1))
-assumptions.append(Not(language.OnLine(B1,CA1)))
+assumptions.append(language.OnLine(F1,FAE))
+assumptions.append(language.OnLine(A1,FAE))
+assumptions.append(language.OnLine(E1,FAE))
 
-# assumptions.append(language.OnLine(P,BC))
-# assumptions.append(language.Angle(P, A, B) == language.Angle(B, C, A))
+assumptions.append(language.Segment(F1, A1) == language.Segment(F1, E1))
 
-# assumptions.append(language.OnLine(Q,BC))
-# assumptions.append(language.Angle(Q, A, C) == language.Angle(C, B, A))
+assumptions.append(language.OnLine(A1,ADC))
+assumptions.append(language.OnLine(D1,ADC))
+assumptions.append(language.OnLine(C1,ADC))
 
-# assumptions.append(language.Segment(P, A) == language.Segment(P, M))
-# assumptions.append(language.Segment(Q, A) == language.Segment(Q, N))
+assumptions.append(language.Segment(E1, B1) == language.Segment(A1, D1))
 
+assumptions.append(language.OnLine(E1,ECB))
+assumptions.append(language.OnLine(B1,ECB))
+assumptions.append(language.OnLine(C1,ECB))
 
-# assumptions.append(language.Center(O, OABC))
-# assumptions.append(language.OnCircle(A, OABC))
-# assumptions.append(language.OnCircle(B, OABC))
-# assumptions.append(language.OnCircle(C, OABC))
+assumptions.append(language.OnLine(A1,AFB))
+assumptions.append(language.OnLine(B1,AFB))
+assumptions.append(language.OnLine(F1,AFB))
 
+assumptions.append(language.OnLine(E1,EFD))
+assumptions.append(language.OnLine(F1,EFD))
+assumptions.append(language.OnLine(D1,EFD))
 
-solver.add(assumptions)
-solver.push()
 print(">> Assume " + str(assumptions))
-solver.add(True)
-res = solver.check()
-print("<< z3: " + str(res))
-if res == unsat:
-    print(solver.unsat_core())
-if res == sat:
-    print(solver.model())
-solver.pop()
+solver.add(assumptions)
+print("<< z3: " + str(solver.check()))
+
+goal = language.Segment(D1, F1) == language.Segment(F1, E1)
+
+print(">> Goal: " + str(goal))
+solver.add(goal)
+print("<< z3: " + str(solver.check()))
+print(solver.model())
+
 
 print("==== Unloading Core ====")
 solver.pop()
